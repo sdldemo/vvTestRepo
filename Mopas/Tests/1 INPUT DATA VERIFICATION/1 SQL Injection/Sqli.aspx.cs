0f07436282b47015d623e1996ba7b60cdbd60155
yisaeva@ptsecurity.ru <yisaeva@ptsecurity.ru>
2016-01-20 14:13:28 +0300

    try fix sql injection

--- Mopas/Tests/1 INPUT DATA VERIFICATION/1 SQL Injection/Sqli.aspx.cs
+++ Mopas/Tests/1 INPUT DATA VERIFICATION/1 SQL Injection/Sqli.aspx.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Data.SqlClient;
 
 namespace Mopas.Tests
@@ -28,7 +28,7 @@ namespace Mopas.Tests
                     // TODO: FP
                     // GET /Tests/1 INPUT DATA VERIFICATION/1 SQL Injection/Sqli.aspx?id=1%27+AND+%271%27%3d%272 HTTP/1.1
                     // Host:localhost
-                    // TODO: AI issue #1, High, SQL Injection, https://github.com/sdldemo/MOPAS/issues/1
+                    // TODO: FP
                     // GET /Tests/1 INPUT DATA VERIFICATION/1 SQL Injection/Sqli.aspx HTTP/1.1
                     // Host:localhost
                     // (System.Convert.ToInt32(this.Request.Params["id"]) == "1' AND '1'='2")
