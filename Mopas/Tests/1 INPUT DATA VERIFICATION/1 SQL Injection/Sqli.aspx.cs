f6f2df7b24eeb66bcb93d4085ab8b37b60ce663d
SDL Demo User <msharonov@ptsecurity.ru>
2016-01-20 14:08:45 +0300

    [SQL] in [Sqli.aspx.cs]

--- Mopas/Tests/1 INPUT DATA VERIFICATION/1 SQL Injection/Sqli.aspx.cs
+++ Mopas/Tests/1 INPUT DATA VERIFICATION/1 SQL Injection/Sqli.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Data.SqlClient;
 
 namespace Mopas.Tests
@@ -28,6 +28,10 @@ namespace Mopas.Tests
                     // TODO: FALSE
                     // GET /Tests/1 INPUT DATA VERIFICATION/1 SQL Injection/Sqli.aspx?id=1%27+AND+%271%27%3d%272 HTTP/1.1
                     // Host:localhost
+                    // TODO: AI issue #1, High, SQL Injection, https://github.com/sdldemo/MOPAS/issues/1
+                    // GET /Tests/1 INPUT DATA VERIFICATION/1 SQL Injection/Sqli.aspx HTTP/1.1
+                    // Host:localhost
+                    // (System.Convert.ToInt32(this.Request.Params["id"]) == "1' AND '1'='2")
                     command.CommandText = string.Format("SELECT test FROM news where id={0}", id);
                     connection.Open();
                     using (var reader = command.ExecuteReader())
