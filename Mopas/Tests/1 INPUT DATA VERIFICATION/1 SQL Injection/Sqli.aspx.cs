87d34d7a57831f48baf038f3e67486bc227a2cd7
yisaeva@ptsecurity.ru <yisaeva@ptsecurity.ru>
2016-01-20 11:41:50 +0300

    first vulnerable commit

---
+++ Mopas/Tests/1 INPUT DATA VERIFICATION/1 SQL Injection/Sqli.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Mopas.Tests
+{
+
+    /// <summary>
+    /// 1.
+    /// SQL Injection
+    /// MOPAS
+    /// Contains 1 vulnerability
+    /// </summary>
+    public partial class Sqli : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string id = Request.Params["id"];
+
+            string str1 = "";
+
+            using (var connection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString))
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "SELECT test FROM news where id=" + id;
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                        str1 += reader["ColumnName"].ToString();
+                }
+            }
+
+            Response.Write("<b>" + str1 + "</b>");
+        }
+    }
+}
---
+++ Mopas/Tests/1 INPUT DATA VERIFICATION/2 Resource Injection/ResourceInjection.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Mopas.Tests
+{
+    /// <summary>
+    /// 2.
+    /// Resource Injection 
+    /// MOPAS
+    /// Contains 1 vulnerability
+    /// </summary>
+    public partial class ResourceInjection : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string fileName = Request.Params["report"];
+
+            if (fileName != null)
+            {
+                File.Delete("D:\\AI\\Reports\\" + fileName);
+            }
+        }
+    }
+}
