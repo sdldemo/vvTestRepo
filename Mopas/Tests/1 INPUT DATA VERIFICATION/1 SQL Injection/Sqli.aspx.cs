57517abf730f59dbb39b3c422e2f09ee8ec8e9ad
yisaeva@ptsecurity.ru <yisaeva@ptsecurity.ru>
2016-01-20 14:05:40 +0300

    try fix sql injection

--- Mopas/Tests/1 INPUT DATA VERIFICATION/1 SQL Injection/Sqli.aspx.cs
+++ Mopas/Tests/1 INPUT DATA VERIFICATION/1 SQL Injection/Sqli.aspx.cs
@@ -14,26 +14,30 @@ namespace Mopas.Tests
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            int id = Convert.ToInt32(Request.Params["id"]);
+                int id = Convert.ToInt32(Request.Params["id"]);
 
-            int str1 = 0;
+                int str1 = 0;
 
-            using (var connection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString))
-            using (var command = connection.CreateCommand())
-            {
-                // TODO: AI issue #1, High, SQL Injection, https://github.com/sdldemo/MOPAS/issues/1
-                // GET /Tests/1 INPUT DATA VERIFICATION/1 SQL Injection/Sqli.aspx?id=1%27+AND+%271%27%3d%272 HTTP/1.1
-                // Host:localhost
-                command.CommandText = string.Format("SELECT test FROM news where id={0}", id);
-                connection.Open();
-                using (var reader = command.ExecuteReader())
+                using (
+                    var connection =
+                        new SqlConnection(
+                            System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"]
+                                .ConnectionString))
+                using (var command = connection.CreateCommand())
                 {
-                    while (reader.Read())
-                        str1 += (int)reader["ColumnName"];
+                    // TODO: FALSE
+                    // GET /Tests/1 INPUT DATA VERIFICATION/1 SQL Injection/Sqli.aspx?id=1%27+AND+%271%27%3d%272 HTTP/1.1
+                    // Host:localhost
+                    command.CommandText = string.Format("SELECT test FROM news where id={0}", id);
+                    connection.Open();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                            str1 += (int) reader["ColumnName"];
+                    }
                 }
-            }
 
-            Response.Write("<b>" + str1 + "</b>");
+                Response.Write("<b>" + str1 + "</b>");
+            }
         }
     }
-}
