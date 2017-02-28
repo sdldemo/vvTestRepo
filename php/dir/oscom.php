20eade8ae8b20691eb06f175ea99baeed5237d46
SDL Demo User <msharonov@ptsecurity.ru>
2017-02-28 16:03:56 +0300

    [OS Commanding] in [oscom.php]

--- php/dir/oscom.php
+++ php/dir/oscom.php
@@ -8,5 +8,13 @@ if (empty($_GET['domain'])) {
 }
 
 $domain = $_GET['domain'];
+// TODO: AI issue #10, High, OS Commanding, https://github.com/sdldemo/vvTestRepo/issues/10
+//
+// GET /php/dir/oscom.php?domain=pwd+%7C%7C+echo+%25cd%25 HTTP/1.1
+// Host: localhost
+// Accept-Encoding: identity
+// Connection: close
+//
+//
 system("nslookup $domain");
 ?>
