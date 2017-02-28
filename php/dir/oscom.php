b9ff2cb6f41e8c79cb8db081929ede8ea5efea09
SDL Demo User <msharonov@ptsecurity.ru>
2017-02-28 16:20:38 +0300

    [OS Commanding] in [oscom.php]

--- php/dir/oscom.php
+++ php/dir/oscom.php
@@ -8,7 +8,7 @@ if (empty($_GET['domain'])) {
 }
 
 $domain = $_GET['domain'];
-// TODO: AI issue #10, High, OS Commanding, https://github.com/sdldemo/vvTestRepo/issues/10
+// FIXED: AI issue #10, High, OS Commanding, https://github.com/sdldemo/vvTestRepo/issues/10
 //
 // GET /php/dir/oscom.php?domain=pwd+%7C%7C+echo+%25cd%25 HTTP/1.1
 // Host: localhost
