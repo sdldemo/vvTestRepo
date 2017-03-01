091eda007674cfa783b25f0d71929c7f25daf941
SDL Demo User <msharonov@ptsecurity.ru>
2017-03-01 13:46:43 +0300

    [XSS] in [xss.php]

--- php/dir/xss.php
+++ php/dir/xss.php
@@ -2,6 +2,13 @@
 /**
  *  MOPAS Cross-site Scripting Example 1
  */
+// TODO: AI issue #27, High, Cross-site Scripting, https://github.com/sdldemo/vvTestRepo/issues/27
+//
+// GET /php/dir/xss.php?a=%3Cscript%3Ealert%281%29%3C%2Fscript%3E HTTP/1.1
+// Host: localhost
+// Accept-Encoding: identity
+// Connection: close
+//
+//
 echo $_GET['a'];
-fopen($_GET['b']);
 ?>
